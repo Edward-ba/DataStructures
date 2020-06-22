7b92f20f02437babb267dbf98b4e50503098ab5e
Edward <ebalasingham@gmail.com>
2020-06-21 20:28:50 -0700

    [Stack] Constructor

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // write your code here
         System.out.println("Hello Data Structures");
 
+        Stack stk = new Stack(10);
+        
     }
 }
--- src/com/company/Stack.java
+++ src/com/company/Stack.java
@@ -1,4 +1,13 @@
 package com.company;
 
 public class Stack {
+
+    /**
+     * Constructor
+     * @param sz - fixed size of stack
+     */
+    Stack(int sz)
+    {
+
+    }
 }
