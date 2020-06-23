b5b8ad11be6b544e14a87317200a27e9eb5c4aab
Edward <ebalasingham@gmail.com>
2020-06-23 15:33:00 -0700

    [Stack] made and finished

--- src/com/company/Stack.java
+++ src/com/company/Stack.java
@@ -1,13 +1,33 @@
 package com.company;
 
 public class Stack {
+    int stack[];
+    int last = 0;
 
-    /**
-     * Constructor
-     * @param sz - fixed size of stack
-     */
-    Stack(int sz)
-    {
-
+    Stack(int sz) {
+        stack = new int[sz];
     }
+
+    public boolean push(int num) {
+        if (last >= stack.length)
+        {
+            return false;
+        }
+        stack[last] = num;
+         ++last;
+        return true;
+    }
+
+    public IntBool pop() {
+        IntBool intBool = new IntBool();
+        if (last == 0)
+        {
+            intBool.bool = false;
+            return intBool;
+        }
+        intBool.num = stack[last];
+        --last;
+        return intBool;
+    }
+
 }
